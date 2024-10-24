ff60e04782d78f0df1ed68d647e3e1cf46a76174
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2024-10-24 13:30:17 -0400

    edit a bit

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -142,19 +142,13 @@ export default function Home() {
       <Header />
 
       <main className="mt-12 flex w-full flex-1 flex-col items-center px-4 text-center sm:mt-20">
-        <a
-          className="mb-4 inline-flex h-7 shrink-0 items-center gap-[9px] rounded-[50px] border-[0.5px] border-solid border-[#E6E6E6] bg-[rgba(234,238,255,0.65)] bg-gray-100 px-7 py-5 shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)]"
-          href="https://dub.sh/together-ai/?utm_source=example-app&utm_medium=llamacoder&utm_campaign=llamacoder-app-signup"
-          target="_blank"
-        >
-          <span className="text-center">
-            Powered by <span className="font-medium">Llama 3.1</span> and{" "}
-            <span className="font-medium">Together AI</span>
-          </span>
-        </a>
         <h1 className="my-6 max-w-3xl text-4xl font-bold text-gray-800 sm:text-6xl">
-          Turn your <span className="text-blue-600">idea</span>
-          <br /> into an <span className="text-blue-600">app</span>
+        </h1>
+        <h1 className="my-6 max-w-3xl text-4xl font-bold text-gray-800 sm:text-6xl">
+        </h1>
+        <h1 className="my-6 max-w-3xl text-4xl font-bold text-gray-800 sm:text-6xl">
+        </h1>
+        <h1 className="my-6 max-w-3xl text-4xl font-bold text-gray-800 sm:text-6xl">
         </h1>
 
         <form className="w-full max-w-xl" onSubmit={createApp}>
@@ -185,63 +179,12 @@ export default function Home() {
                 </button>
               </div>
             </div>
-            <div className="mt-6 flex flex-col justify-center gap-4 sm:flex-row sm:items-center sm:gap-8">
-              <div className="flex items-center justify-between gap-3 sm:justify-center">
-                <p className="text-gray-500 sm:text-xs">Model:</p>
-                <Select.Root
-                  name="model"
-                  disabled={loading}
-                  value={model}
-                  onValueChange={(value) => setModel(value)}
-                >
-                  <Select.Trigger className="group flex w-60 max-w-xs items-center rounded-2xl border-[6px] border-gray-300 bg-white px-4 py-2 text-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-500">
-                    <Select.Value />
-                    <Select.Icon className="ml-auto">
-                      <ChevronDownIcon className="size-6 text-gray-300 group-focus-visible:text-gray-500 group-enabled:group-hover:text-gray-500" />
-                    </Select.Icon>
-                  </Select.Trigger>
-                  <Select.Portal>
-                    <Select.Content className="overflow-hidden rounded-md bg-white shadow-lg">
-                      <Select.Viewport className="p-2">
-                        {models.map((model) => (
-                          <Select.Item
-                            key={model.value}
-                            value={model.value}
-                            className="flex cursor-pointer items-center rounded-md px-3 py-2 text-sm data-[highlighted]:bg-gray-100 data-[highlighted]:outline-none"
-                          >
-                            <Select.ItemText asChild>
-                              <span className="inline-flex items-center gap-2 text-gray-500">
-                                <div className="size-2 rounded-full bg-green-500" />
-                                {model.label}
-                              </span>
-                            </Select.ItemText>
-                            <Select.ItemIndicator className="ml-auto">
-                              <CheckIcon className="size-5 text-blue-600" />
-                            </Select.ItemIndicator>
-                          </Select.Item>
-                        ))}
-                      </Select.Viewport>
-                      <Select.ScrollDownButton />
-                      <Select.Arrow />
-                    </Select.Content>
-                  </Select.Portal>
-                </Select.Root>
-              </div>
+              
 
-              <div className="flex h-full items-center justify-between gap-3 sm:justify-center">
-                <label className="text-gray-500 sm:text-xs" htmlFor="shadcn">
-                  shadcn/ui:
-                </label>
-                <Switch.Root
-                  className="group flex w-20 max-w-xs items-center rounded-2xl border-[6px] border-gray-300 bg-white p-1.5 text-sm shadow-inner transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-500 data-[state=checked]:bg-blue-500"
-                  id="shadcn"
-                  name="shadcn"
-                  checked={shadcn}
-                  onCheckedChange={(value) => setShadcn(value)}
-                >
-                  <Switch.Thumb className="size-7 rounded-lg bg-gray-200 shadow-[0_1px_2px] shadow-gray-400 transition data-[state=checked]:translate-x-7 data-[state=checked]:bg-white data-[state=checked]:shadow-gray-600" />
-                </Switch.Root>
-              </div>
+            <div className="mt-6 flex flex-col justify-center gap-4 sm:flex-row sm:items-center sm:gap-8">
+              <div>Import Git Repository</div>
+
+              <div>Clone a Template</div>
             </div>
           </fieldset>
         </form>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,61 @@
 import Image from "next/image";
-import Link from "next/link";
-import logo from "../public/logo.png";
-import GithubIcon from "./github-icon";
+import emberLogo from "../public/emberLogo.png";
+import bellIcon from "../public/bell.png"
+import memoIcon from "../public/memo.png"
 
 export default function Header() {
   return (
     <header className="relative mx-auto mt-5 flex w-full items-center justify-center px-2 pb-7 sm:px-4">
-      <Link href="/" className="absolute flex items-center gap-2">
-        <Image alt="header text" src={logo} className="h-5 w-5" />
-        <h1 className="text-xl tracking-tight">
-          <span className="text-blue-600">Llama</span>Coder
-        </h1>
-      </Link>
+      <Image alt="Ember" src={emberLogo} style={{height:30, width:'auto'}}  />
+
+      &nbsp; Christian's workspace
+
       <a
-        href="https://github.com/nutlope/llamacoder"
+        href=""
         target="_blank"
         className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
       >
-        <GithubIcon className="h-4 w-4" />
-        <span>GitHub Repo</span>
+        <span>Upgrade</span>
+      </a>
+
+      <a
+        href=""
+        target="_blank"
+        className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
+      >
+        <span>Feedback</span>
+      </a>
+
+      <a
+        href=""
+        target="_blank"
+        className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
+      >
+        <span>Changelog</span>
+      </a>
+
+      <a
+        href=""
+        target="_blank"
+        className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
+      >
+        <span>Support</span>
+      </a>
+
+      <a
+        href=""
+        target="_blank"
+        className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
+      >
+        <Image alt="bell" src={bellIcon} style={{height:25, width:'auto'}} />
+      </a>
+
+      <a
+        href=""
+        target="_blank"
+        className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
+      >
+        <Image alt="memo" src={memoIcon} style={{height:25, width:'auto'}} />
       </a>
     </header>
   );
